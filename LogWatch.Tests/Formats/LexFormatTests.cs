df662cd39f77b539d12041f3e57363fd23bf2a0f
Sergey Shumov <shumov.ss@gmail.com>
2013-04-17 23:16:29 +0400

    started to implement Lex log format

---
+++ LogWatch.Tests/Formats/LexFormatTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading;
+using LogWatch.Formats;
+using Xunit;
+using System.Reactive.Linq;
+using System.Reactive.Threading.Tasks;
+
+namespace LogWatch.Tests.Formats {
+    public class LexFormatTests {
+        [Fact(Timeout = 60000)]
+        public void ReadsSegments() {
+            var stream = CreateStream("01.01.2012T15:41:23 DEBUG Hello world!\r\n02.01.2012T10:23:03 WARN Bye bye!");
+            const string lex = @"
+timestamp   [0-9]{2}[.][0-9]{2}[.][0-9]{4}[T][0-9]{2}[:][0-9]{2}[:][0-9]{2}
+level       TRACE|DEBUG|INFO|WARN|ERROR|FATAL
+message     [^\r\n]+
+
+record      {timestamp}[ ]{level}[ ]{message}\r\n
+";
+
+            var format = new LexLogFormat {
+                Diagnostics = Console.Out, 
+                SegmentsExpression = lex
+            };
+
+            format.TryCompileSegmentsScanner();
+
+            var subject = new ReplaySubject<RecordSegment>();
+
+            format.ReadSegments(subject, stream, CancellationToken.None).Wait();
+
+            var segment = subject.FirstAsync().ToTask().Result;
+
+            stream.Position = segment.Offset;
+
+            var buffer = new byte[segment.Length];
+
+            stream.Read(buffer, 0, buffer.Length);
+
+            var str = Encoding.UTF8.GetString(buffer);
+
+            Assert.Equal("01.01.2012T15:41:23 DEBUG Hello world!\r\n", str);
+        }
+
+        [Fact]
+        public void DeserializesRecord() {
+            const string lex = @"
+timestamp   [0-9]{2}[.][0-9]{2}[.][0-9]{4}[T][0-9]{2}[:][0-9]{2}[:][0-9]{2}
+level       TRACE|DEBUG|INFO|WARN|ERROR|FATAL
+message     [^\r\n]+
+
+%x matched_level
+%%
+
+<INITIAL,matched_level> {
+    {timestamp} { this.Timestamp = yytext; BEGIN(INITIAL); }
+    {level} { this.Level = yytext; BEGIN(matched_level); }
+}
+<matched_level>{message} { this.Message = yytext; BEGIN(INITIAL); }
+";
+
+            var format = new LexLogFormat {
+                Diagnostics = Console.Out,
+                RecordsExpression = lex
+            };
+
+            Assert.True(format.TryCompileRecordsScanner());
+
+            var record = format.DeserializeRecord(
+                new ArraySegment<byte>(Encoding.UTF8.GetBytes("01.01.2012T15:41:23 DEBUG Hello world!\r\n")));
+
+            Assert.Equal(LogLevel.Debug, record.Level);
+            Assert.Equal(" Hello world!", record.Message);
+        }
+
+        private static MemoryStream CreateStream(string content) {
+            var bytes = Encoding.UTF8.GetBytes(content);
+            var stream = new MemoryStream(bytes);
+            return stream;
+        } 
+    }
+}
---
+++ LogWatch/Formats/LexLogFormat.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Roslyn.Compilers;
+using Roslyn.Compilers.CSharp;
+
+namespace LogWatch.Formats {
+    public class LexLogFormat : ILogFormat {
+        private const string ScannerBase = @"
+            using System;
+            using System.Collections.Generic;
+            
+            namespace LogWatch.Formats.Lex {
+                public abstract class ScanBase {
+                    public Action<long, int> OffsetCallback;
+
+                    protected void NextOffset(long offset, int length) {
+                        OffsetCallback(offset, length);
+                    }
+
+                    public abstract int yylex();
+
+                    protected virtual bool yywrap() {
+                        return true;
+                    }
+
+                    public string Timestamp { get; set; }            
+                    public string Level { get; set; }
+                    public string Logger { get; set; }
+                    public string Message { get; set; }
+                    public string Exception { get; set; }
+                }
+
+                public enum Tokens {
+                    EOF = 0,
+                    maxParseToken = int.MaxValue
+                }
+            }
+        ";
+
+        private const string LexSegmentsFooter = @"
+            %namespace LogWatch.Formats.Lex
+
+            %%
+
+            {record} this.NextOffset((long) yypos, yyleng);";
+        private readonly ThreadLocal<dynamic> reocordsScanner;
+
+        private Type recordsScannerType;
+        private Type segmentsScannerType;
+
+        public LexLogFormat() {
+            this.reocordsScanner = new ThreadLocal<dynamic>(() => Activator.CreateInstance(this.recordsScannerType));
+        }
+
+        public string SegmentsExpression { get; set; }
+        public string RecordsExpression { get; set; }
+
+        public TextWriter Diagnostics { get; set; }
+
+        public Record DeserializeRecord(ArraySegment<byte> segment) {
+            var scanner = this.reocordsScanner.Value;
+
+            scanner.SetSource((Stream) new MemoryStream(segment.Array, segment.Offset, segment.Count));
+            scanner.yylex();
+
+            return new Record {
+                Level = GetLevel(scanner.Level),
+                Logger = scanner.Logger,
+                Message = scanner.Message,
+                Exception = scanner.Exception
+            };
+        }
+
+        public Task<long> ReadSegments(
+            IObserver<RecordSegment> observer,
+            Stream stream,
+            CancellationToken cancellationToken) {
+            return Task.Factory.StartNew(() => {
+                dynamic scanner = Activator.CreateInstance(this.segmentsScannerType);
+
+                scanner.OffsetCallback =
+                    new Action<long, int>((offset, length) => observer.OnNext(new RecordSegment(offset, length)));
+
+                scanner.SetSource(stream);
+                scanner.yylex();
+
+                return -1L;
+            });
+        }
+
+        public bool CanRead(Stream stream) {
+            return true;
+        }
+
+        private static LogLevel? GetLevel(string levelString) {
+            if (levelString == null)
+                return null;
+
+            switch (levelString.ToLower()) {
+                case "trace":
+                    return LogLevel.Trace;
+                case "debug":
+                    return LogLevel.Debug;
+                case "info":
+                    return LogLevel.Info;
+                case "warn":
+                    return LogLevel.Warn;
+                case "error":
+                    return LogLevel.Error;
+                case "fatal":
+                    return LogLevel.Fatal;
+                default:
+                    return null;
+            }
+        }
+
+        private static Type Compile(string lexCode, TextWriter diagnostics, string typeName) {
+            var lexFile = Path.ChangeExtension(Path.GetTempFileName(), ".lex");
+            var codeFile = Path.ChangeExtension(lexFile, ".cs");
+
+            File.WriteAllText(lexFile, lexCode, Encoding.UTF8);
+
+            var process = Process.Start(new ProcessStartInfo {
+                FileName = "gplex.exe",
+                Arguments = string.Format("/verbose /version /noPersistBuffer /unicode /out:{0} {1}", codeFile, lexFile),
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            });
+
+            process.WaitForExit();
+
+            diagnostics.Write(process.StandardOutput.ReadToEnd());
+            diagnostics.Write(process.StandardError.ReadToEnd());
+
+            var code = File.ReadAllText(codeFile, Encoding.UTF8);
+
+            var codeTree = SyntaxTree.ParseText(code);
+            var scannerTree = SyntaxTree.ParseText(ScannerBase);
+
+            foreach (var diagnostic in codeTree.GetDiagnostics())
+                diagnostics.WriteLine(diagnostic);
+
+            var mscorlib = MetadataReference.CreateAssemblyReference("mscorlib");
+
+            var name = string.Format("LexFileFormat-{0}", Guid.NewGuid().ToString("N"));
+
+            var compilation = Compilation.Create(
+                outputName: name,
+                syntaxTrees: new[] {codeTree, scannerTree},
+                references: new[] {mscorlib},
+                options: new CompilationOptions(OutputKind.DynamicallyLinkedLibrary, optimize: true));
+
+            var assembly = AppDomain.CurrentDomain.DefineDynamicAssembly(
+                new AssemblyName(name), AssemblyBuilderAccess.RunAndCollect);
+
+            var module = assembly.DefineDynamicModule(name);
+
+            var result = compilation.Emit(module);
+
+            foreach (var diagnostic in result.Diagnostics)
+                diagnostics.WriteLine(diagnostic);
+
+            return assembly.GetType(typeName);
+        }
+
+        public bool TryCompileRecordsScanner() {
+            var lexCode = string.Join(
+                Environment.NewLine,
+                "%namespace LogWatch.Formats.Lex",
+                this.RecordsExpression);
+
+            this.recordsScannerType = Compile(lexCode, this.Diagnostics, "LogWatch.Formats.Lex.Scanner");
+
+            return this.recordsScannerType != null;
+        }
+
+        public bool TryCompileSegmentsScanner() {
+            if (!this.SegmentsExpression.Contains("{record}")) {
+                this.Diagnostics.WriteLine("Expected 'record' token definition");
+                return false;
+            }
+
+            var lexCode = string.Join(
+                Environment.NewLine,
+                new[] {this.SegmentsExpression}
+                    .Concat(LexSegmentsFooter
+                        .Split(new[] {Environment.NewLine}, StringSplitOptions.None)
+                        .Select(x => x.Trim())));
+
+            this.segmentsScannerType = Compile(lexCode, this.Diagnostics, "LogWatch.Formats.Lex.Scanner");
+
+            return this.segmentsScannerType != null;
+        }
+    }
+}
