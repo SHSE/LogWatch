92cc7922e80d9e0b8721b6e58ebab61d8db16bb2
Sergey Shumov <shumov.ss@gmail.com>
2013-05-10 01:52:14 +0400

    added xunit to the repository

--- default.ps1
+++ default.ps1
@@ -8,24 +8,7 @@ properties {
 
 task default -depends Build
 
-task Download-XUnit {
-    if (Test-Path .\Tools\xunit\xunit.console.clr4.x86.exe) {
-        return;
-    }
-
-    New-Item .\Tools\xunit -ItemType Directory -ErrorAction SilentlyContinue
-    
-    $wc = New-Object System.Net.WebClient
-    $wc.DownloadFile('http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=xunit&DownloadId=423827&FileTime=129859153262930000&Build=20474', '.\Tools\xunit\xunit.zip')
-
-    $shell_app = new-object -com shell.application
-    $zip_file = $shell_app.namespace((Resolve-Path .\Tools\xunit\xunit.zip).Path)
-    $destination = $shell_app.namespace((Resolve-Path .\Tools\xunit).Path)
-    $destination.Copyhere($zip_file.items())
-    
-}
-
-task Test -depends Download-XUnit {
+task Test {
     exec { msbuild .\LogWatch.Tests\LogWatch.Tests.csproj /p:DownloadNuGetExe=True }
     exec { .\Tools\xunit\xunit.console.clr4.x86.exe .\LogWatch.Tests\bin\Debug\LogWatch.Tests.dll /teamcity }
 }
