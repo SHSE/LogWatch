b0b5dd95f51ff792c7cd6bb9f317e608779574f4
Sergey Shumov <shumov.ss@gmail.com>
2013-09-02 14:55:55 +0400

    added ability to import and export Lex presets

--- LogWatch/Features/Formats/LexPresetsView.xaml.cs
+++ LogWatch/Features/Formats/LexPresetsView.xaml.cs
@@ -1,16 +1,39 @@
-﻿using System;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Win32;
 
 namespace LogWatch.Features.Formats {
     public partial class LexPresetsView {
-        public static readonly Func<bool> ConfirmDelete =
-            () => ShowMessage("Are you sure?", "Delete", MessageBoxButton.YesNo) == true;
-
         public LexPresetsView() {
             this.InitializeComponent();
 
             this.Buttons = new[] {this.OkButton, this.CancelButton};
             this.OkButton.Content = "select";
+
+            this.ViewModel.SelectFileForImport = () => {
+                var dialog = new OpenFileDialog {
+                    Filter = "LogWatch Lex Presets|*.lwlex|All Files|*.*",
+                    CheckFileExists = true
+                };
+
+                if (dialog.ShowDialog() == true)
+                    return dialog.FileName;
+
+                return null;
+            };
+
+            this.ViewModel.SelectFileForExport = () => {
+                var dialog = new SaveFileDialog {
+                    Filter = "LogWatch Lex Presets|*.lwlex|All Files|*.*",
+                    OverwritePrompt = true
+                };
+
+                if (dialog.ShowDialog() == true)
+                    return dialog.FileName;
+
+                return null;
+            };
+
+            this.ViewModel.ConfirmDelete = () => ShowMessage("Are you sure?", "Delete", MessageBoxButton.YesNo) == true;
         }
 
         public LexPresetsViewModel ViewModel {
--- LogWatch/Features/Formats/LexPresetsViewModel.cs
+++ LogWatch/Features/Formats/LexPresetsViewModel.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Runtime.CompilerServices;
+using System.Xml;
+using System.Xml.Linq;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using LogWatch.Annotations;
@@ -46,9 +48,54 @@ namespace LogWatch.Features.Formats {
                 },
                 () => this.selectedPreset != null);
 
-            if (this.IsInDesignMode) {
+            this.ExportCommand = new RelayCommand(
+                () => Export(this.SelectedPreset),
+                () => this.SelectedPreset != null);
+            
+            this.ImportCommand = new RelayCommand(this.Import);
+
+            if (this.IsInDesignMode)
                 this.Presets.Add(new LexPreset {Name = "New Preset"});
+        }
+
+        public Func<string> SelectFileForExport { get; set; }
+        public Func<string> SelectFileForImport { get; set; }
+
+        private void Export(LexPreset preset) {
+            var fileName = SelectFileForExport();
+
+            if (fileName == null)
                 return;
+
+            var document = new XDocument(
+                new XElement("Preset",
+                    new XAttribute("Name", preset.Name),
+                    new XElement("Common", preset.CommonCode),
+                    new XElement("Segment", preset.SegmentCode),
+                    new XElement("Record", preset.RecordCode)));
+
+            document.Save(fileName);
+        }
+
+        private void Import() {
+             var fileName = SelectFileForImport();
+
+            if (fileName == null)
+                return;
+
+            try {
+                var document = XDocument.Load(fileName).Root ?? new XElement("Preset");
+
+                var preset = new LexPreset {
+                    Name = (string) document.Attribute("Name"),
+                    CommonCode = (string) document.Element("Common"),
+                    SegmentCode = (string) document.Element("Segment"),
+                    RecordCode = (string) document.Element("Record")
+                };
+
+                this.Presets.Add(preset);
+            } catch (XmlException exception) {
+                throw new ApplicationException("Invalid file format", exception);
             }
         }
 
@@ -68,6 +115,7 @@ namespace LogWatch.Features.Formats {
                 this.EditCommand.RaiseCanExecuteChanged();
                 this.DuplicateCommand.RaiseCanExecuteChanged();
                 this.DeleteCommand.RaiseCanExecuteChanged();
+                this.ExportCommand.RaiseCanExecuteChanged();
             }
         }
 
@@ -75,6 +123,8 @@ namespace LogWatch.Features.Formats {
         public RelayCommand EditCommand { get; set; }
         public RelayCommand DuplicateCommand { get; set; }
         public RelayCommand DeleteCommand { get; set; }
+        public RelayCommand ExportCommand { get; set; }
+        public RelayCommand ImportCommand { get; set; }
 
         [NotifyPropertyChangedInvocator]
         private void OnPropertyChanged([CallerMemberName] string propertyName = null) {
